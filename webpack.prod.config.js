c565f6d048949747a84744d21d47b400e95b18bc
Axel Benitez <benitez3313@gmail.com>
2025-05-05 00:21:10 -0700

    feat: Configuraciones Agregadas
    Se agregaron configuraciones necesarias para hacer deploy

---
+++ webpack.prod.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
